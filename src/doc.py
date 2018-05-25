4fce209afa17a436c472de097ade390f7c1ed469
Tim Menzies <tim.menzies@gmail.com>
2018-05-24 22:22:14 -0400

    update

--- src/doc.py
+++ src/doc.py
@@ -1,13 +1,25 @@
 from ethel import *
+import contextlib
 
 def globs():
   header=""
+  headers=[]
+  lines=[]
   for k,v in globals().items():
      if callable(v):
          if str(v.__module__) != header:
+           if v.__module__:
+            if v.__module__ not in  ["contextlib","__main__"]:
              header=v.__module__
-             print("\n#",header,"\n")
+             lines += [["\n_______\n\n#<a name=\"",header,"\">",header,"</a>\n"]]
+             headers += [header]
          if v.__doc__:
-              print("\n## ",k,"\n\n",v.__doc__,"\n",sep="")
+              doc = v.__doc__
+              doc = re.sub(r'\n[ \t]*', "\n", doc)
+              lines += [["\n## ",k,"\n\n",doc,"\n"]]
+  for h in headers:
+    print(h)
+  for line in lines:
+    print(*line,sep="")
 
 globs()
