540010f52ed2a32eb083445644290966ab003d7b
Tim Menzies <tim.menzies@gmail.com>
2018-06-16 23:35:01 -0400

    update

--- src/sandbox.py
+++ src/sandbox.py
@@ -1,3 +1,5 @@
+import re
+
 def once(f):
   s0 = "_" + f.__name__
   def wrapped(i,*l,**k):
@@ -22,8 +24,6 @@ class Cache:
     tmp = super().__new__(i,*l,**kw)
     tmp._cache= {}
     return tmp
-
-
 
 class Fred (Cache):
   def __init__(i,a=1): 
@@ -54,3 +54,73 @@ for i in range(6,100,15):
   if i == 6:
     x.__dict__[('_fib',2)] = None
 print(x)
+
+class Xpect(object):
+  all = {}
+  def __init__(i, act=None, prompt=None, flag=None, 
+                  ako=None, default=None):
+    i.act,i.flag,i.ako,  i.val,i.prompt = \
+     None,  flag,  ako,default,  prompt
+  def ftype(i):
+    tmp = i.ako if callable(i.ako) else type(i.ako[0])
+    return tmp.__name__.upper()
+  def bad(i,x):
+    if isinstance(i.ako,list):
+      if x not in i.ako:
+        return '%s not one of %s' % (x,i.ako)
+    else:
+      if not i.ako(x)
+        return '%s is not %s' % i.ako.__name__
+  @staticmethod
+  def call(x):
+    meta = Xpect.all[x]
+    f    = meta[0]
+    args = [y.val for y in meta[1:]]
+    return f(*args)
+  def __repr__(i):
+    return str(i.__dict__)
+  def help(x):
+    meta = Xpect.all[x]
+    name = meta[0].__name__
+    doc  = "  "+re.sub(r'\n[ \t]*', "\n  ",meta[0].__doc__)
+    args = meta[1:]
+    print('%s\n\n%s\n' % (name,doc))
+    show = [('--%s' % y.flag, y.ftype(), y.prompt,str(y.val)) 
+            for y in args]
+    lens  = [[len(one) for one in trio] for  trio in show]
+    widths= ''.join(['  %%-%ss' % max(*m) for m in zip(*lens)])
+    [print(widths % one) for one in show]
+
+def flags(**kw):
+  def decorator(f):
+    s = f.__name__
+    holder = Xpect.all[s] = [f]
+    for k in kw:
+      maker = kw[k]
+      maker.flag = k
+      holder += [ maker ]
+    return f
+  return decorator
+
+import os
+def filep(f) : return os.exist(f)
+def posint(f): return int(f) and f >= 0
+
+String = lambda x,y: Xpect(ako= str,   default=x,    prompt=y)
+Int    = lambda x,y: Xpect(ako= int,   default=x,    prompt=y)
+Float  = lambda x,y: Xpect(ako= float, default=x,    prompt=y) 
+File   = lambda x,y: Xpect(ako= filep, default=x,    prompt=y)
+Bool   = lambda x,y: Xpect(ako= bool,  default=x,    prompt=y)
+OneOf  = lambda x,y: Xpect(ako= x,     default=x[0], prompt=y)
+
+@flags(what    = String("world","print string"), 
+       repeats = Int(3,"number of repeats"))
+def hello(what, repeats):
+  """documentation for this function can
+   spill over many
+   lines"""
+  for _ in range(repeats):
+     print('Hello %s' % what)
+
+Xpect.call("hello")
+Xpect.help("hello")
