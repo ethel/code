13fb06ceb66cf8fa53433fa9b60cd867a0a31deb
Tim Menzies <tim.menzies@gmail.com>
2018-06-28 17:12:24 -0400

    update

--- tests/test.sh
+++ tests/test.sh
@@ -1,2 +1,12 @@
-cd ../src
-time python3 ethel.py
+# All tests are held in the source code (see any function decorated with @ok).
+
+# These tests can be run as follows.
+
+cd ..
+python3 ethel --check
+
+
+# The last line shows the count of failing/passing tests. e.g.
+#
+# PASS= 11 FAIL= 0 %PASS = 100%
+
